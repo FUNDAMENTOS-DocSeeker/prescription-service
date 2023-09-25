c7caaf9c0285584b4764c38aa4fea277f7eb878e
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-09-25 12:12:57 -0500

    feat: add test runner

---
+++ src/test/java/com/dockseeker/prescription/CucumberTestRunner.java
@@ -0,0 +1,13 @@
+package com.dockseeker.prescription;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = { "com.dockseeker.prescription.cucumberglue" }
+)
+public class CucumberTestRunner {
+}
